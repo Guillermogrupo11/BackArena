d6a29225cde896097ec15e762f103519a33e1171
Guillermo <guillermo.arena@gmail.com>
2022-10-10 15:45:09 -0300

    Update IPersonaRepository.java

--- src/main/java/com/portfolio/gda/Repository/IPersonaRepository.java
+++ src/main/java/com/portfolio/gda/Repository/IPersonaRepository.java
@@ -1,7 +1,9 @@
 
 package com.portfolio.gda.Repository;
 
+
 import com.portfolio.gda.Entity.Persona;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,5 +12,7 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface IPersonaRepository extends JpaRepository<Persona,Integer> {
+    public Optional<Persona> findByNombre(String nombre);
+    public boolean existsByNombre(String nombre);
     
 }
