2468459998f7f5a1329b899df6068796cc7995fc
Guillermo <guillermo.arena@gmail.com>
2022-10-10 15:21:18 -0300

    Update AuthController.java

--- src/main/java/com/portfolio/gda/Security/Controller/AuthController.java
+++ src/main/java/com/portfolio/gda/Security/Controller/AuthController.java
@@ -36,8 +36,8 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping("/auth")
-@CrossOrigin(origins = "https://frontendgda.web.app")
-//@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins ={"https://frontendgda.web.app","http://localhost:4200"})
+
 public class AuthController {
     @Autowired
     PasswordEncoder passwordEncoder;
